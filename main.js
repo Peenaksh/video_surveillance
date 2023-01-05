2f017c406195020f317d07dc5b94686942dd20cd
Peenaksh <85746966+Peenaksh@users.noreply.github.com>
2023-01-05 17:44:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 video = ""
-
+objects = []
+status = ""
 function setup(){
     canvas = createCanvas(480,380)
     canvas.center()
@@ -12,16 +13,41 @@ function preload() {
 
 function start() {
     objectDetector = ml5.objectDetector('cocoSSD',modelloaded)
-    document.getElementById("status").innerHTML = "STATUS: DETECTING OBJECTS"
+    document.getElementById("status").innerHTML = "STATUS: DETECTING OBJECTS";
 
 }
 function draw() {
   image(video,0,0,480,380);
+  if(status != ""){
+    objectDetector.detect(video,gotResult)
+    for(i=0 ; i<objects.length ; i++) {
+        document.getElementById("status").innerHTML = "STATUS: OBJECT DETECTED"
+        document.getElementById("no_of_objects").innerHTML = "NO. OF OBJECTS DETECTED = " + objects.length;
+        fill("#3b021e")
+        
+        percent = floor(objects[i].confiedence*100)
+        text(objects[i].label + " " + percent + "%", objects[i].x + 15,objects[i].y + 15)
+        noFill();
+        stroke("#02063b")
+        rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height)
+    }
+  }
 
 }
 function modelloaded() {
     console.log("model is initialised");
     video.speed(1)
-    video.volume(0)
+    video.volume(1)
     video.loop()
+}
+function gotResult(results,error) {
+    if(error){
+        console.log(error)
+    }
+    else {
+        console.log(results);
+        objects = results;
+    }
+
+    
 }
